dcd2338f34003618e3729572a9258fba2572f538
Billy Cao <aliencaocao@gmail.com>
2022-04-13 20:15:25 +0800

    Updates

--- cyberthon training/defuse me.py
+++ cyberthon training/defuse me.py
@@ -1,7 +1,7 @@
 from pwn import *
 
-# io = remote('chals.cyberthon22t.ctf.sg', 30101)
-io = process('./defuse_me')
+io = remote('chals.cyberthon22t.ctf.sg', 30101)
+#io = process('./defuse_me')
 
 # lvl 1
 lvl1 = [0x1201C743, 0x536F83AF, 0x6C77C87B, 0xE48CA057]
@@ -16,9 +16,9 @@ pause()
 
 # lvl 3 v1
 v2 = b'0'  # gives eax=2, works
-v3 = b'1'  # gives eax=3, shld give 1
+v3 = b'\x01'  # gives eax=3, shld give 1
 v4 = b'a'  # gives eax=3 ,works
-v5 = b'2'  # gives eax=3, shld give 2
+v5 = b'0'  # gives eax=3, shld give 2
 v6 = b'3'  # gives eax=4, works
 io.sendline(v2 + v3 + v4 + v5 + v6)
 
--- cyberthon training/main.py
+++ cyberthon training/main.py
@@ -1,13 +1,62 @@
-from pwn import *
-from random import randint
-from binascii import hexlify
+import sys
+import logging
 
-KEY_LENGTH = 16
-KEY = bytearray([randint(0, 255) for _ in range(KEY_LENGTH)])
-print(KEY)
+from flask import Flask, jsonify
+import json
 
-steve = open('steve.txt', 'rb').read()
-dave = open('dave.txt', 'rb').read()
+app = Flask(__name__)
+flag = open("flag.txt").read().strip()
 
-print(f'Steve: {hexlify(xor(steve, KEY)).decode()}')
-print(f'Dave: {hexlify(xor(dave, KEY)).decode()}')
+# country_code = {}
+# with open("countries.json") as f:
+#     country_code = json.loads(f.read())
+country_code = {"china":"CN"}
+
+class Country(object):
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+
+
+countries = {x: Country(x, country_code[x]) for x in country_code.keys()}
+
+
+@app.route("/", methods=["GET"])
+def home():
+    data = {
+        "specific country code": "/api/v1/:country:",
+        "all country codes": "/api/v1/all",
+    }
+    return jsonify(data)
+
+
+@app.route("/api/v1/all", methods=["GET"])
+def all_codes():
+    return jsonify([{"Name": x.name, "Code": x.code} for x in countries.values()])
+
+
+@app.route("/api/v1/<string:country>", methods=["GET"])
+def get_code(country):
+    try:
+        print(country)
+        print(f'{country.lower()}')
+        print(f"{{{country.lower()}.code}}".format(**countries))
+        #print(country.lower().code)
+        data = {
+            "Name": country.lower(),
+            "Code": f"{{{country.lower()}.code}}".format(**countries),
+        }
+    except OSError as e:
+        print(e)
+        data = {"error": "country not found!"}
+
+    return jsonify(data)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    data = {"error": str(e)}
+
+    return jsonify(data)
+
+app.run('localhost', 8080)
